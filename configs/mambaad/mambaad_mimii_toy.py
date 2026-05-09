00700ac358326086f0eb1ed5616f5daf51e14c91
algasami <105358814+algasami@users.noreply.github.com>
2026-05-09 11:11:23 +0800

    lower epoch, lower test frequency, increase batch size

--- configs/mambaad/mambaad_mimii_toy.py
+++ configs/mambaad/mambaad_mimii_toy.py
@@ -18,11 +18,11 @@ class cfg(cfg_common, cfg_dataset_default, cfg_model_mambaad):
 		self.fvcore_c = 3
 		self.seed = 42
 		self.size = 256
-		self.epoch_full = 1000
+		self.epoch_full = 300
 		self.warmup_epochs = 0
 		self.test_start_epoch = self.epoch_full
-		self.test_per_epoch = self.epoch_full // 20
-		self.batch_train = 16  # official 16
+		self.test_per_epoch = self.epoch_full // 10
+		self.batch_train = 64
 		self.batch_test_per = 16
 		self.lr = 0.005 * self.batch_train / 16
 		self.weight_decay = 0.0001
